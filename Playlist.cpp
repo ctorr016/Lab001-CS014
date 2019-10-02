77aa17f0ddd546589306bd879a4946195e8107c3
ctorr016 <46534859+ctorr016@users.noreply.github.com>
2019-10-02 10:44:17 -0700

    Update Playlist.cpp

--- Playlist.cpp
+++ Playlist.cpp
@@ -3,3 +3,51 @@ using namespace std;
 
 #include "Playlist.h"
 
+Playlist::Playlist(string PlaylistTitle, PlaylistNode* nextNode)
+{
+  
+}
+
+void Playlist::InsertAfter(PlaylistNode* nodePtr)
+{
+  
+}
+
+void Playlist::SetNext()
+{
+  
+}
+
+string Playlist::GetID()
+{
+    return uniqueID;
+}
+
+string Playlist::GetSongName()
+{
+    return songName;
+}
+
+string Playlist::GetArtistName()
+{
+    return artistName;
+}
+
+int Playlist::GetSongLength()
+{
+    return songLength;
+}
+
+PlaylistNode* Playlist::GetNext()
+{
+    return nextNodePtr;
+}
+
+void Playlist::PrintPlaylistNode()
+{
+    cout << "Unique ID: " << uniqueID << endl;
+    cout << "Song Name: " << songName << endl;
+    cout << "Artist Name: " << artistName << endl;
+    cout << "Song Length (in seconds): " << songLength << endl;
+}
+
