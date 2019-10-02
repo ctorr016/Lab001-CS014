57c1f441a5975c85e29e6cff2ab3439786005ae3
ctorr016 <46534859+ctorr016@users.noreply.github.com>
2019-10-02 09:13:51 -0700

    Update Playlist.h

--- Playlist.h
+++ Playlist.h
@@ -1,1 +1,24 @@
+#ifndef PLAYLIST_H
+#define PLAYLIST_H
 
+class Playlist
+{
+    public:
+      Playlist(string PlaylistTitle, PlaylistNode* nextNode = nullptr);
+      void InsertAfter(PlaylistNode* nodePtr);
+      void SetNext();
+      string GetID(string ID);
+      string GetSongName(string SongName);
+      string GetArtistName(string ArtistName);
+      int GetSongLength(int SongLength);
+      PlaylistNode* GetNext();
+      void PrintPlaylistNode();
+    private:
+      string uniqueID = "none";
+      string songName = "none";
+      string artistName = "none";
+      int songLength = 0;
+      PlaylistNode* nextNodePtr = 0;
+}
+  
+#endif
